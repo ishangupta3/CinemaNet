b81674ba7df788f450eceed5e27e9dfb34e7f317
vade <vade@vade.info>
2019-06-08 14:40:45 -0400

    Update our auto labeler with new features, can now output an html preview of a randomly sampled subset of images so we can better understand the label output.

--- auto_labeler.py
+++ auto_labeler.py
@@ -3,13 +3,17 @@ import csv
 import argparse
 import PIL.Image
 import coremltools
+import random
 
 parser = argparse.ArgumentParser(description='Use a folder of ML model classifiers to label a local unlabeled data set')
 
 parser.add_argument('-m', '--modeldir', type=str, help='folder containing core ml models to use as labelers. Each image to label will be run through each model', default='./models', required=True)
 parser.add_argument('-i', '--imagedir', type=str, help="folder containing unlabeled images to be labeled", default="./images", required=True)
-parser.add_argument('-o', '--output', type=str, help="destination for labeled CSV file containing multi labels", default="./labels.csv", required=True)
+parser.add_argument('-o', '--output', type=str, help="destination for labeled file containing multi labels", default="./labels", required=True)
+parser.add_argument('-t', '--type', type=str, help="csv or html?", default="csv", required=False)
 parser.add_argument('-pre', '--prefix', type=str, help="image url prefix, useful for adding a cloud storage provider URL for example", default="", required=False)
+parser.add_argument('-l', '--limit', type=int, help="limit the number of images we label - useful for testing", default="", required=False)
+parser.add_argument('-r', '--random', type=bool, help="limit the number of images we label - useful for testing", default=False, required=False)
 
 args = parser.parse_args()
 
@@ -62,14 +66,53 @@ def load_image(path, resize_to=None):
 
 	return img
 
-# open a csv file for writing
+def html_header(): 
+	html_header = """
+	<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+
+	<html>
+
+		<head>
+
+		<title>Conforming HTML 4.01 Transitional Template</title>
+
+		</head>
+
+	<body>
+	"""
+	return html_header
+
+def html_entry(filepath, labels):
+	html_entry = """
+	<div align="center">
+	<img src={} width="200px" height="200px" /> {}
+	</div>
+	"""
+
+	return html_entry.format(filepath, ', '.join(labels) )
+
+
+def html_footer():
+	html_footer = """
+	</body>
+
+	</html>
+	"""
+	return html_footer
+
+# open a file for writing
 
 # for reference, for multi label we want to do
 # : gs://calm-trees-123-vcm/flowers/images/5217892384_3edce91761_m.jpg,dandelion,tulip,rose
 # from https://cloud.google.com/vision/automl/docs/prepare
+all_files = []
+with open(args.output, 'wb') as writer:
 
-with open(args.output, 'wb') as f:
-	writer = csv.writer(f)
+	if args.type is 'csv':
+ 		writer = csv.writer(writer)
+ 	else:
+ 		writer.write(html_header())
 
 	# recurse through our image directory and run inference on each image
 	for subdir, dirs, files in os.walk(args.imagedir):
@@ -78,26 +121,44 @@ with open(args.output, 'wb') as f:
 			filepath = subdir + os.sep + file
 
 			if filepath.endswith(".jpg"):
-				image = load_image(filepath, resize_to=(Width, Height))
+				all_files.append(filepath)
 
-				if image is not None:
-					labels = []
+	#do we shuffle our files?
+	if args.random is True:
+		random.shuffle(all_files)
 
-					# prepend our prefix if we have it
-					if args.prefix:
-						labels.append( args.prefix + file )
-					else:
-						labels.append(file)
+	#do we limit our file count so we can do a test run?
+	if args.limit is not 0:
+		all_files = all_files[:args.limit]
 
-					for model in models:
-						prediction = model.predict({'image__0': image})
-						label = prediction['classLabel']
-						labels.append(label)
+	for filepath in all_files:
+		image = load_image(filepath, resize_to=(Width, Height))
+		if image is not None:
+			labels = []
 
-					#write all of our predictions out to our CSV
-					writer.writerow(labels)
-					print("labeled " + file)
+			# prepend our prefix if we have it
+			if args.prefix:
+				labels.append( args.prefix + filepath )
+			else:
+				labels.append(filepath)
 
+			for model in models:
+				prediction = model.predict({'image__0': image})
+				label = prediction['classLabel']
+				labels.append(label)
+
+			#write all of our predictions out to our CSV
+			if args.type is 'csv':
+				writer.writerow(labels)
+			else:
+			# write HTML label version with file name for IMG tag, etc
+				writer.write( html_entry(filepath, labels) )
+
+			print("labeled " + filepath)
+
+
+if args.type is 'html':
+ 		writer.writer(html_footer())
 
 
 
